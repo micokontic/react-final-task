03c0e2914bab8ffd484fbedc2d274635c1e383cb
Mico Kontic <micokontic@gmail.com>
2021-02-15 12:00:07 +0100

    blob data added

--- src/components/FlipCard.js
+++ src/components/FlipCard.js
@@ -3,7 +3,8 @@ import './FlipCard.css'
 import bonfireFull from './images/bonfire_full.svg';
 import bonfireEmpty from './images/bonfire_empty.svg';
 import locationPin from './images/location.svg';
-function FlipCard() {
+function FlipCard({activitie}) {
+  console.log(activitie)
     return (
 <div className="flip-card-container">
   <div className="flip-card">
@@ -11,13 +12,13 @@ function FlipCard() {
     <div className="card-front">
       <figure>
         <div className="img-bg"></div>
-        <img src="https://bluerivertara.me/me/img/blue-river-tara-9.jpg" alt="Brohm Lake"></img>
-        <figcaption>Splavarenje Tarom</figcaption>
+      <img src={activitie.images[0]} alt="Brohm Lake"></img>
+        <figcaption>{activitie.title}</figcaption>
       </figure>
 
       <ul>
         <li><h3><span className='price-tag'>50$</span> per person</h3></li>
-        <li><div className='location-continer'><img src={locationPin}></img><div className='location-text'>Tara River</div></div></li>
+        <li><div className='location-continer'><img src={locationPin}></img><div className='location-text'>{activitie.location}</div></div></li>
         <li><div className='bonfire-rating-container'>
               <div className='bonfire-rating'>
                   <img src={bonfireFull}/>
@@ -34,7 +35,7 @@ function FlipCard() {
     <div className="card-back">
       <figure>
         <div className="img-bg"></div>
-        <img src="https://bluerivertara.me/me/img/blue-river-tara-9.jpg" alt="Brohm Lake"></img>
+        <img src={activitie.images[0]} alt="Brohm Lake"></img>
       </figure>
 
       <button>Book now</button>
--- src/components/FlipCards.js
+++ src/components/FlipCards.js
@@ -1,12 +1,35 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
 import FlipCard from './FlipCard'
 import './FlipCards.css'
 function FlipCards() {
+    
+    const [activities,setActivities]=useState([])
+
+    useEffect(() => {
+        getBlob();
+    }, [])
+
+    const getBlob=()=>{ 
+        /*ovdje bi stavio url tog koji ti treba*/
+        fetch("https://jsonblob.com/api/jsonBlob/d98ac18e-6a1c-11eb-b59d-a157b950443c") 
+              .then(response=>{
+             return response.json();
+              })
+        .then(response=>{
+            setActivities(response);
+        })
+              .catch(function(error){
+                  console.log(error);
+              });
+      }
+    
+
     return (
         <div className='flip-cards-container'>
-            <FlipCard/>
-            <FlipCard/>
-            <FlipCard/>
+            {activities.map((activitie)=>{
+                return(<FlipCard activitie={activitie}/>)
+            })}
+            
 
         </div>
     )
