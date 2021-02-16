bf165ccb4d7d101667e11c79ce53413251683c3c
Vasilije Knezevic <vasilije.upwork@gmail.com>
2021-02-16 17:10:24 +0100

    Host registration

--- src/components/pages/forms/registrationForm/FormCallRegister.js
+++ src/components/pages/forms/registrationForm/FormCallRegister.js
@@ -2,7 +2,10 @@ import React, { useState } from "react";
 import FormRegister from "./FormRegister";
 import FormSuccess from "./FormSuccess";
 import FormLogin from "./FormLogin";
+import { GiMountainRoad } from "react-icons/gi";
 import "./Form.css";
+import { Button } from "../../../../Button";
+import { Link } from "react-router-dom";
 const Form = () => {
   const [isSubmitted, setIsSubmitted] = useState(false);
 
@@ -14,7 +17,17 @@ const Form = () => {
       <div className="form-wrapper">
         <div className="form-container">
           <span className="close-btn">×</span>
-          <div className="form-content-left"></div>
+          <div className="form-content-left">
+            <div className="form">
+              <h1 className="joinTeamh1">Join Our Team!</h1>
+              <GiMountainRoad className="mountainIcon" />
+              <p className="form-storyP">Be part of our story!</p>
+
+              <Link to="/registerHost">
+                <Button buttonStyle="btn--primary">Become a member</Button>
+              </Link>
+            </div>
+          </div>
           {!isSubmitted ? (
             <FormRegister submitForm={submitForm} />
           ) : (
