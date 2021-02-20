25c6e6f7e97528291d6c81fd7e7c477c41ea247a
Mico Kontic <micokontic@gmail.com>
2021-02-20 10:14:07 +0100

    new changes after merge

--- src/components/FlipCards.js
+++ src/components/FlipCards.js
@@ -1,52 +1,40 @@
-import React, { useEffect, useState } from "react";
-import FlipCard from "./FlipCard";
-import WrappedMap from "./Map.js";
-import "./FlipCards.css";
-import getCoordintesFromActivities from "./CoordinateFunctions";
+import React,{useEffect,useState} from 'react'
+import FlipCard from './FlipCard'
+import WrappedMap from './Map.js'
+import './FlipCards.css'
 function FlipCards() {
-  const [activities, setActivities] = useState([]);
-  const [coordinates, setCoordinates] = useState([]);
+    
+    const [activities,setActivities]=useState([])
 
   useEffect(() => {
     getBlob();
   }, []);
 
-  const getBlob = () => {
-    /*ovdje bi stavio url tog koji ti treba*/
-    fetch(
-      "https://jsonblob.com/api/jsonBlob/d98ac18e-6a1c-11eb-b59d-a157b950443c"
-    )
-      .then((response) => {
-        return response.json();
-      })
-      .then((response) => {
-        setActivities(response);
-        setCoordinates(getCoordintesFromActivities(response));
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  };
+    const getBlob=()=>{ 
+        /*ovdje bi stavio url tog koji ti treba*/
+        fetch("https://jsonblob.com/api/jsonBlob/d98ac18e-6a1c-11eb-b59d-a157b950443c") 
+              .then(response=>{
+             return response.json();
+              })
+        .then(response=>{
+            setActivities(response);
 
-  return (
-    <>
-      <div className="flip-cards-section">
-        <div className="flip-cards-container">
-          {activities.map((activitie) => {
-            return <FlipCard activitie={activitie} />;
-          })}
-        </div>
+        })
+              .catch(function(error){
+                  console.log(error);
+              });
+      }
+    
 
-        <div style={{ width: "30vw", height: "60vh" }}>
-          <WrappedMap
-            googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyBnLRlCsIutdZMmkPI51Z3sIxG-CRW7qGw`}
-            loadingElement={<div style={{ height: "100%" }} />}
-            containerElement={<div style={{ height: "100%" }} />}
-            mapElement={<div style={{ height: "100%" }} />}
-            Markers={coordinates}
-          />
+    return (
+        <>
+        <div className='flip-cards-section'>
+            <div className='flip-cards-container'>
+                {activities.map((activitie)=>{
+                    return(<FlipCard activitie={activitie} height={'400px'} size={'big'}/>)
+                })}
+            </div>
         </div>
-      </div>
     </>
   );
 }
