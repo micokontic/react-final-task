faf96981b9bbe53357b1d9e2519b8e0695ccc954
Mico Kontic <micokontic@gmail.com>
2021-02-26 22:37:48 +0100

    backgroundAdded

--- src/components/AdventureCard/AdventureCard.js
+++ src/components/AdventureCard/AdventureCard.js
@@ -35,7 +35,7 @@ function AdventureCard({activitie,type}) {
             <div>
             </div>:
             <div className="user-container zoom">
-               <UserCard/>
+               <UserCard type={type}/>
             </div>
             }
 
--- src/components/UserCard/UserCard.js
+++ src/components/UserCard/UserCard.js
@@ -2,17 +2,19 @@ import React from 'react'
 import "./UserCard.css"
 import GuideLogo from "../images/guide.svg"
 
-function UserCard() {
+function UserCard({type}) {
     return (
         <div className="user-card-container">
             <div className="profile-img zoom">
                 <img className='image' src="https://widgetwhats.com/app/uploads/2019/11/free-profile-photo-whatsapp-4-300x300.png"></img>
             </div>
-            <div className='profile-details'>
+            <div className='profile-details' style={type==="user"?{"grid-template-columns": "1fr"}:''}>
+                {type==="user"?'':
                 <div className='guide-logo-container'>
                     <img className='guide-logo-img' src={GuideLogo}></img>
                     <h5 style={{display:'block'}}>GUIDE</h5>
                 </div>
+                }
                 <h4>Marko Markovic</h4>
             </div>
         </div>
