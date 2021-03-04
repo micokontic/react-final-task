74a29f7040d6def26250e756a667edfa6e77625d
Vasilije Knezevic <vasilije.upwork@gmail.com>
2021-03-04 16:18:57 +0100

    Register forms flipped

--- src/components/pages/forms/registrationForm/FormRegister.js
+++ src/components/pages/forms/registrationForm/FormRegister.js
@@ -23,6 +23,106 @@ function FormRegister({ submitForm }) {
         <form
           className={hideForm ? "hideForm" : "form"}
           onSubmit={handleSubmit}
+          id="userForm"
+        >
+          <h1>Join us today!</h1>
+          <div className="form-inputs">
+            <label htmlFor="username" className="form-label">
+              Username:{" "}
+            </label>
+            <input
+              id="usernameUser"
+              type="text"
+              name="username"
+              placeholder="Enter your username"
+              className="form-input"
+              value={values.username}
+              onChange={handleChange}
+            />
+            {errors.username && <p>{errors.username}</p>}
+          </div>
+
+          <div className="form-inputs">
+            <label htmlFor="email" className="form-label">
+              Email:{" "}
+            </label>
+            <input
+              id="emailUser"
+              type="email"
+              name="email"
+              placeholder="Enter your email"
+              className="form-input"
+              value={values.email}
+              onChange={handleChange}
+            />
+            {errors.email && <p>{errors.email}</p>}
+          </div>
+
+          <div className="form-inputs">
+            <label htmlFor="password" className="form-label">
+              password:{" "}
+            </label>
+            <input
+              id="passwordUser"
+              type="password"
+              name="password"
+              placeholder="Enter your password"
+              className="form-input"
+              value={values.password}
+              onChange={handleChange}
+            />
+            {errors.password && <p>{errors.password}</p>}
+          </div>
+
+          <div className="form-inputs">
+            <label htmlFor="password2" className="form-label">
+              Confirm password:{" "}
+            </label>
+            <input
+              id="password2User"
+              type="password"
+              name="password2"
+              placeholder="Confirm your password"
+              className="form-input"
+              value={values.password2}
+              onChange={handleChange}
+            />
+            {errors.password2 && <p>{errors.password2}</p>}
+          </div>
+
+          <Button buttonStyle="btn--primary" id="userSubmit" type="submit">
+            Register
+          </Button>
+          <span className="form-input-login">
+            Already have an account? Login <Link to="/log-in">here</Link>
+          </span>
+        </form>
+        <div className={hideForm ? "form" : "hideForm"}>
+          <h1 className="joinTeamh1">host!</h1>
+
+          <div className="form-svg traveller"></div>
+          <p className="form-storyP">tekst za hosta</p>
+          <Link to="/registerHost">
+            <Button buttonStyle="btn--primary">Enter</Button>
+          </Link>
+        </div>
+      </div>
+
+      <div className={hideForm ? "form-content-right" : "form-content-left"}>
+        <div className={hideForm ? "hideForm" : "form"}>
+          <h1 className="joinTeamh1">korisnik!</h1>
+          <div className="form-svg host"></div>
+
+          <p className="form-storyP">tekst za korisnika!</p>
+
+          <Link to="/registerHost">
+            <Button buttonStyle="btn--primary">Enter</Button>
+          </Link>
+        </div>
+
+        <form
+          className={hideForm ? "form" : "hideForm"}
+          onSubmit={handleSubmit}
           id="hostForm"
         >
           <h1>Host!</h1>
@@ -95,106 +195,6 @@ function FormRegister({ submitForm }) {
             />
             {errors.phoneNumber && <p>{errors.phoneNumber}</p>}
           </div>
-
-          <Button buttonStyle="btn--primary" id="registerHost" type="submit">
-            Register
-          </Button>
-          <span className="form-input-login">
-            Already have an account? Login <Link to="/log-in">here</Link>
-          </span>
-        </form>
-        <div className={hideForm ? "form" : "hideForm"}>
-          <h1 className="joinTeamh1">korisnik!</h1>
-
-          <div className="form-svg traveller"></div>
-          <p className="form-storyP">tekst za korisnika</p>
-          <Link to="/registerHost">
-            <Button buttonStyle="btn--primary">Enter</Button>
-          </Link>
-        </div>
-      </div>
-
-      <div className={hideForm ? "form-content-right" : "form-content-left"}>
-        <div className={hideForm ? "hideForm" : "form"}>
-          <h1 className="joinTeamh1">host!</h1>
-          <div className="form-svg host"></div>
-
-          <p className="form-storyP">tekst za hosta!</p>
-
-          <Link to="/registerHost">
-            <Button buttonStyle="btn--primary">Enter</Button>
-          </Link>
-        </div>
-
-        <form
-          className={hideForm ? "form" : "hideForm"}
-          onSubmit={handleSubmit}
-          id="userForm"
-        >
-          <h1>Join us today!</h1>
-          <div className="form-inputs">
-            <label htmlFor="username" className="form-label">
-              Username:{" "}
-            </label>
-            <input
-              id="usernameUser"
-              type="text"
-              name="username"
-              placeholder="Enter your username"
-              className="form-input"
-              value={values.username}
-              onChange={handleChange}
-            />
-            {errors.username && <p>{errors.username}</p>}
-          </div>
-
-          <div className="form-inputs">
-            <label htmlFor="email" className="form-label">
-              Email:{" "}
-            </label>
-            <input
-              id="emailUser"
-              type="email"
-              name="email"
-              placeholder="Enter your email"
-              className="form-input"
-              value={values.email}
-              onChange={handleChange}
-            />
-            {errors.email && <p>{errors.email}</p>}
-          </div>
-
-          <div className="form-inputs">
-            <label htmlFor="password" className="form-label">
-              password:{" "}
-            </label>
-            <input
-              id="passwordUser"
-              type="password"
-              name="password"
-              placeholder="Enter your password"
-              className="form-input"
-              value={values.password}
-              onChange={handleChange}
-            />
-            {errors.password && <p>{errors.password}</p>}
-          </div>
-
-          <div className="form-inputs">
-            <label htmlFor="password2" className="form-label">
-              Confirm password:{" "}
-            </label>
-            <input
-              id="password2User"
-              type="password"
-              name="password2"
-              placeholder="Confirm your password"
-              className="form-input"
-              value={values.password2}
-              onChange={handleChange}
-            />
-            {errors.password2 && <p>{errors.password2}</p>}
-          </div>
           <Button buttonStyle="btn--primary" id="userSubmit" type="submit">
             Register
           </Button>
@@ -208,9 +208,9 @@ function FormRegister({ submitForm }) {
           <label id="button" htmlFor="lol-checkbox">
             <div id="knob" />
 
-            <div id="subscribe">User?</div>
+            <div id="subscribe">Host?</div>
 
-            <div id="alright">Host?</div>
+            <div id="alright">User?</div>
           </label>
         </div>
       </div>
