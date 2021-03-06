33ddc29e4d6f0911275ad56b5c824e22b11714a0
Vasilije Knezevic <vasilije.upwork@gmail.com>
2021-03-06 18:14:40 +0100

    many new styles and animations added

--- src/components/pages/AboutUs/AboutUs.js
+++ src/components/pages/AboutUs/AboutUs.js
@@ -4,18 +4,20 @@ import { Link } from "react-router-dom";
 function AboutUs() {
   return (
     <>
-      <div className="aboutUsContainer">
-        <Link to="/frontEndTeam" className="linkFront">
-          <div className="frontEnd">
-            <h1 className="frontTeam teams">FRONTEND TEAM</h1>
-          </div>
-        </Link>
+      <div className="aboutUsWrapper">
+        <div className="aboutUsContainer">
+          <Link to="/frontEndTeam" className="linkFront">
+            <div className="frontEnd">
+              <h1 className="frontTeam teams">FRONTEND TEAM</h1>
+            </div>
+          </Link>
 
-        <Link to="/backEndTeam" className="linkBack">
-          <div className="backEnd">
-            <h1 className="backTeam teams">BACKEND TEAM</h1>
-          </div>
-        </Link>
+          <Link to="/backEndTeam" className="linkBack">
+            <div className="backEnd">
+              <h1 className="backTeam teams">BACKEND TEAM</h1>
+            </div>
+          </Link>
+        </div>
       </div>
     </>
   );
